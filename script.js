6d430c38c833c6d9a802fcc5fb9488ada03b63d3
Anunymux <jonjohnsonwright@gmail.com>
2017-03-01 14:11:14 +0100

    added delete, pending, completed

--- script.js
+++ script.js
@@ -2,23 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: '#vue-app',
         data: vueVars,
-        methods: vueMethods
+        methods: vueMethods,
+        computed: vueComputedProperties
     })
 })
 
 var vueVars = {
-    todos: [{ id: 1, title: 'Go Shopping' },
-    { id: 2, title: 'new Todo' }],
-    newTodo: { id: null, title: '' }
+    todos: [{ id: 1, title: 'Go Shopping', completed: false }],
+    newTodo: { id: null, title: '', completed: false }
 }
 
 var vueMethods = {
 
     addNewTodo(newTodo) {
         vueVars.todos.push(newTodo)
-        vueVars.newTodo = { id: null, title: '' }
+        vueVars.newTodo = { id: null, title: '', completed: false }
     },
     deleteTodo(todo) {
         vueVars.todos.splice(vueVars.todos.indexOf(todo, -1))
+    },
+
+    todoCompleted(todo) {
+        todo.completed = !todo.completed
+    }
+}
+
+var vueComputedProperties = {
+    todoCount() {
+        if (vueVars.todos.length <= 5) {
+            return vueVars.todos.length
+        } else {
+            return 'Too many!'
+        }
     }
 }
